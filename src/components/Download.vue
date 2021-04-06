1d622315ee3095b9ca7bf4b4e52f0cee5ca4ce90
Yancy Min <senseslofter@gmail.com>
2021-04-06 12:16:41 +0800

    96.4

--- src/components/Download.vue
+++ src/components/Download.vue
@@ -342,7 +342,7 @@ export default {
           cursor: pointer;
 
           &::before {
-              content: "当前支持 Figma 版本号：95.8";
+              content: "当前支持 Figma 版本号：96.4";
               position: absolute;
               bottom: -40px;
               padding: 8px;
